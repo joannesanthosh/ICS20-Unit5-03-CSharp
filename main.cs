adf7b935c7123bb4319b09e1658aecac062fd2c0
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-06-11 20:35:41 +0000

    fix spacing

--- main.cs
+++ main.cs
@@ -9,22 +9,21 @@ class Program
 {
     public static void Main(string[] args)
     {
-      int age;
+        int age;
 
-      Console.WriteLine("Please enter your age:");
-      age = int.Parse(Console.ReadLine());
-      if (age > 17) {
-      Console.WriteLine("You are eligible to watch an R-rated movie.");  
-      }
-      else if (age > 13) {
-      Console.WriteLine("You're eligible to watch a PG-13 movie."); 
-      }
-      else if (age > 5) {
-      Console.WriteLine("You can see a G or PG movie.");
-      }
-      else {
-      Console.WriteLine("You're too young to watch most things.");
-      }
-      Console.WriteLine("Done.");
+        // input
+        Console.WriteLine("Please enter your age:");
+        age = int.Parse(Console.ReadLine());
+
+        // process
+        if (age > 17) 
+        Console.WriteLine("You are eligible to watch an R-rated movie.");  
+        else if (age > 13) 
+        Console.WriteLine("You're eligible to watch a PG-13 movie."); 
+        else if (age > 5) 
+        Console.WriteLine("You can see a G or PG movie.");
+        else 
+        Console.WriteLine("You're too young to watch most things.");
+        Console.WriteLine("Done.");
     }
 }
